e41a6a7f74e071fb7565967ebcdaf148d5a9b1f0
Xakse Xakse <roma.roma.romanov2001@gmail.com>
2025-01-03 16:45:37 +0300

    adas

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+  this.direct = direct;
+}
+encrypt(message, key) {
+  if (!message || !key || message.trim() === '' || key.trim() === '') {
+    throw new Error('Incorrect arguments!');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  const uppercaseMessage = message.toUpperCase();
+  const uppercaseKey = key.toUpperCase();
+  let result = [];
+  let keyIndex = 0;
+  for (let i = 0; i < message.length; i++) {
+    let char = uppercaseMessage[i];
+    if (/[A-Z]/.test(char)) {
+      const shift = uppercaseKey.charCodeAt(keyIndex % uppercaseKey.length) - 65;
+      const code = (uppercaseMessage.charCodeAt(i) + shift - 65)  % 26 + 65;
+      result.push(String.fromCharCode(code));
+      keyIndex++; 
+    }else result.push(char);
   }
+  return this.direct ? result.join('') : result.reverse().join('');
+}
+decrypt(message, key) {
+  if (!message || !key || message.trim() === '' || key.trim() === '') {
+    throw new Error('Incorrect arguments!');
+  }
+  const uppercaseMessage = message.toUpperCase();
+  const uppercaseKey = key.toUpperCase();
+  let result = [];
+  let keyIndex = 0;
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+    if (/[A-Z]/.test(char)) {
+      const shift = uppercaseKey.charCodeAt(keyIndex % uppercaseKey.length) - 65;
+      const code = (uppercaseMessage.charCodeAt(i) - shift - 65 + 26)  % 26 + 65;
+      result.push(String.fromCharCode(code));
+      keyIndex++; 
+    }else result.push(char);
+  }
+  return this.direct ? result.join('') : result.reverse().join('');
+}
 }
 
 module.exports = {
